135f812c72335c0fb8e49dc4e1814dadf2e9aade
zainiftikhar20 <zainiftikhar20@gmail.com>
2020-11-26 03:38:45 +0500

    Complete BMI Calculator || lec # 70 || part 16

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:bmi_calculator/Input_Page.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -9,8 +9,8 @@ class BMICalculator extends StatelessWidget {
     return MaterialApp(
       home: InputPage(),
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        primaryColor: Color(0xAFF0A0E21),
+        scaffoldBackgroundColor: Color(0xAFF0A0E21),
       ),
     );
   }
